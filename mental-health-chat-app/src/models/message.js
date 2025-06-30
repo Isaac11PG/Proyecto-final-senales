a72a92b8550ee7a0cad99321e3df7b3eb748e24f
IsaacPG11 <137459133+Isaac11PG@users.noreply.github.com>
2025-06-30 17:19:16 -0600

    Elminados

--- mental-health-chat-app/src/models/message.js
+++
@@ -1,9 +0,0 @@
-class Message {
-    constructor(userId, content) {
-        this.userId = userId;
-        this.content = content;
-        this.timestamp = new Date();
-    }
-}
-
-module.exports = Message;
